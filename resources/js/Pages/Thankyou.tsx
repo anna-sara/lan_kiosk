f3ed9c4d990b762317abe48654a5ac5dc748e05b
Anna-Sara Sélea <anna-sara@qualitythink.se>
2025-04-21 11:23:19 +0200

    Added swish link to Thankyou view

--- resources/js/Pages/Thankyou.tsx
+++ resources/js/Pages/Thankyou.tsx
@@ -10,8 +10,9 @@ export default function Thankyou() {
             <section className='section'>
                 <div className="container is-max-desktop">
                     <div className="box px-5 py-5">
-                        <h1 className="title px-3 py-3">Tack! Deltagaren registrerad</h1>
-                        <p className="subtitle is-5 px-3 py-3">Swisha eller betala in en önskad summa pengar och meddela kioskpersonal.</p>
+                        <h1 className="title px-3 py-3">Tack! Deltagaren är registrerad</h1>
+                        <p className="subtitle is-5 px-3 py-3">Swisha in en önskad summa pengar och meddela kioskpersonal eller betala in en önskad summa pengar i kontanter till kioskpersonal.</p>
+                        <a className='button' href="swish://">Öppna swish</a>
                     </div>
                 </div>
             </section>
