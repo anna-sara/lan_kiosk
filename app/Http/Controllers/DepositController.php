188303be93368407e47c46a234f8905e58aee788
Anna-Sara Sélea <anna-sara@qualitythink.se>
2025-05-05 18:31:38 +0200

    Deposit model, controller, migration

---
+++ app/Http/Controllers/DepositController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Deposit;
+use App\Models\Customer;
+use Illuminate\Http\Request;
+
+class DepositController extends Controller
+{
+     /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $deposits = Deposit::get();
+        return $deposits->toJson();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request, $id)
+    {
+        $request->validate([
+            'deposit' => 'required',
+        ]);
+
+        Deposit::create([
+            'customer_id' => $id,
+            'amount' => $request->deposit,
+        ]);
+
+        $customer = Customer::findOrFail($id);
+        $customer->deposit = $customer->deposit + $request->deposit;
+        $customer->amount_left = $customer->amount_left + $request->deposit;
+        $customer->save();
+
+        return redirect('customer/' . $customer->id);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Purchase $purchase)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Purchase $purchase)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Purchase $purchase)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        $deposit = Deposit::findOrFail( $id );
+        $customer = Customer::where('id', $deposit->customer_id);
+        $customer->amount_left =  $customer->amount_left + $deposit->amount;
+        $customer->amount_used =  $customer->amount_used - $deposit->amount;
+        $customer->save();
+        $deposit->delete();
+    
+        return response()->json([
+            'success' => true, 'message' => 'Deposit deleted successfully'
+        ]);
+    }
+}
