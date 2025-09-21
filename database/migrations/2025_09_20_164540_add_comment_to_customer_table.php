f68a18c6b3dcbbcddb62c7427c9e530a8381966b
Anna-Sara Sélea <anna-sara@qualitythink.se>
2025-09-21 08:43:11 +0200

    Added comment to customer model. Comment in frontend

--- database/migrations/2025_09_20_164540_add_comment_to_customer_table.php
+++ database/migrations/2025_09_20_164540_add_comment_to_customer_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('customers', function (Blueprint $table) {
-            $table->mediumText('comment');
+            $table->mediumText('comment')->nullable();
         });
     }
 
